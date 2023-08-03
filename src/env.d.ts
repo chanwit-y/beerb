7b0ab4e30bc9a98328ac8c9b26cdd8ae14260460
cy <chanwit.yimneam@hotmail.com>
2023-08-03 09:22:20 +0700

    setup firebase

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly API_KEY: string;
+  readonly AUTH_DOMAIN: string;
+  readonly PROJECT_ID: string;
+  readonly STORAGE_BUCKET: string;
+  readonly MESSAGING_SENDER_ID: string;
+  readonly APP_ID: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ src/lib/config/env.ts
@@ -0,0 +1,8 @@
+export const Env = {
+  API_KEY: import.meta.env.PUBLIC_API_KEY,
+  AUTH_DOMAIN: import.meta.env.PUBLIC_AUTH_DOMAIN,
+  PROJECT_ID: import.meta.env.PUBLIC_PROJECT_ID,
+  STORAGE_BUCKET: import.meta.env.PUBLIC_STORAGE_BUCKET,
+  MESSAGING_SENDER_ID: import.meta.env.PUBLIC_MESSAGING_SENDER_ID,
+  APP_ID: import.meta.env.PUBLIC_APP_ID,
+};
