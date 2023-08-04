99bf208d8bc1f2e4f659950fe7c7eefb21465178
cy <chanwit.yimneam@hotmail.com>
2023-08-04 20:31:44 +0700

    image list

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), svelte()]
+  integrations: [react(), tailwind(), svelte(), image()]
 });
--- src/lib/components/ImageBlurhash.tsx
+++ src/lib/components/ImageBlurhash.tsx
@@ -1,33 +1,56 @@
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import { Blurhash } from 'react-blurhash';
 
 export const ImageBlurhash = () => {
 
-	const width: number = 334;
-	const height: number = 225;
+	const width: number = 798;
+	const height: number = 532;
 
+	const [loaded, setLoaded] = useState(false);
 
+	useEffect(() => {
+		console.log(loaded)
+	}, [loaded])
 
 	return (
 
 		<motion.div
-			initial={{ opacity: 0, scale: 0.5 }}
-			animate={{ opacity: 1, scale: 1 }}
+			style={{
+				width: width,
+				height: height,
+			}}
+			// initial={{ opacity: 0, scale: 0.5 }}
+			// animate={{ opacity: 1, scale: 1 }}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
 			transition={{ duration: 0.3 }}
 		>
-			<Blurhash
-				// style={{ borderRadius: '10px', overflow: 'hidden'}}
-				className="h-52 w-52 m-2 p-1 rounded-md overflow-hidden "
-				hash="U5CuX;yG00k4-;xcbcIB00%w.hV#DhDOMy-="
+			<img
 
-			// width={400}
-			// height={300}
-			// resolutionX={32}
-			// resolutionY={32}
-			// punch={1}
+				style={{
+					width: width,
+					height: height,
+					display: !loaded ? 'none' : 'block'
+				}}
+				onLoadStart={() => {
+					console.log('onLoadStart')
+				}}
+				src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F462f9f0c-b635-4606-bdcc-9e4d14f1bbbb.jpeg?alt=media&token=ee8103e0-4ba6-47c8-8992-e2cb43ab2c76"
+				onLoad={() => {
+					setLoaded(true)
+				}}
 			/>
-			{/* <Image /> */}
+			<Blurhash
 
+				style={{
+					width: width,
+					height: height,
+					display: loaded ? 'none' : 'block'
+				}}
+				className="m-2 p-1 rounded-md overflow-hidden "
+				hash="U5CuX;yG00k4-;xcbcIB00%w.hV#DhDOMy-="
+			/>
 		</motion.div>
 	)
 }
